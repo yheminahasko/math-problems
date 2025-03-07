4ca27ab51312f82475a6b9b55b545733ad002fdb
yheminahasko <yheminahasko@gmail.com>
2025-03-07 13:49:21 +0000

    Add 2.js

---
+++ src/2.js
@@ -0,0 +1,14 @@
+const mathProblems = {
+    problem1: {
+        question: "What is 2 + 2?",
+        answer: 4
+    },
+    problem2: {
+        question: "What is 5 x 5?",
+        answer: 25
+    },
+    problem3: {
+        question: "What is the sum of 1, 2, and 3?",
+        answer: 6
+    }
+}
