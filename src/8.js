25561513eaf9851f37f3749eb265c1c3dc029e6a
yheminahasko <yheminahasko@gmail.com>
2025-03-13 11:21:28 +0000

    Add 8.js

---
+++ src/8.js
@@ -0,0 +1,14 @@
+var a = 5;
+var b = 8;
+var c = 3;
+
+// Find the area of the triangle using the formula:
+// area = (base * height) / 2
+var area = (a * b) / 2;
+
+// Find the perimeter of the triangle using the formula:
+// perimeter = a + b + c
+var perimeter = a + b + c;
+
+console.log("The area of the triangle is " + area);
+console.log("The perimeter of the triangle is " + perimeter);
