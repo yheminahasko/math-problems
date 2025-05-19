97ec83d3995f7fbb46b40e48715c648834200b8e
yheminahasko <yheminahasko@gmail.com>
2025-05-19 03:40:27 +0000

    Add 41.js

---
+++ src/41.js
@@ -0,0 +1,11 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+
+function calculateTotal() {
+    let sum = 0;
+    for (let i = 1; i <= 100; i++) {
+        sum += i;
+    }
+    console.log("The sum of the first 100 natural numbers is: " + sum);
+}
